bbd57c881e7282b34b31ea9d5b57ab50fc8cc728
Hiroaki-K4 <49no19@gmail.com>
2024-12-01 21:03:25 -0800

    Acclerate octree

--- srcs/Octree.cpp
+++ srcs/Octree.cpp
@@ -11,7 +11,7 @@ Octree::Octree(glm::vec3 min_3d_coord, glm::vec3 max_3d_coord) {
 
 Octree::~Octree() {}
 
-bool Octree::is_point_inside(Node &node, glm::vec3 position) {
+bool Octree::is_point_inside(Node &node, const glm::vec3 &position) {
     if (position.x < node.min_bound.x || position.x > node.max_bound.x ||
         position.y < node.min_bound.y || position.y > node.max_bound.y ||
         position.z < node.min_bound.z || position.z > node.max_bound.z) {
@@ -22,15 +22,11 @@ bool Octree::is_point_inside(Node &node, glm::vec3 position) {
 
 void Octree::insert(std::vector<glm::vec3> &position, std::vector<float> &mass) {
     for (int i = 0; i < position.size(); i++) {
-        // TODO: Fix insert function because it is too slow.
-        if (i % 1000 == 0) {
-            std::cout << "i: " << i << std::endl;
-        }
         insert_point(this->root, position[i], mass[i]);
     }
 }
 
-void Octree::insert_point(Node &node, glm::vec3 position, float mass) {
+void Octree::insert_point(Node &node, const glm::vec3 &position, float mass) {
     if (!is_point_inside(node, position)) {
         return;
     }
@@ -44,10 +40,21 @@ void Octree::insert_point(Node &node, glm::vec3 position, float mass) {
         node.is_empty = false;
     } else {
         if (node.is_leaf) {
+            // TODO: Set max points threshold
             subdivide(node);
+            for (auto& child : node.children) {
+                if (is_point_inside(child, node.position)) {
+                    insert_point(child, node.position, node.mass);
+                    break;
+                }
+            }
+            node.is_leaf = false;
         }
         for (auto& child : node.children) {
-            insert_point(child, position, mass);
+            if (is_point_inside(child, position)) {
+                insert_point(child, position, mass);
+                break;
+            }
         }
     }
 }
@@ -57,61 +64,60 @@ void Octree::subdivide(Node &node) {
                 (node.min_bound.y + node.max_bound.y) / 2,
                 (node.min_bound.z + node.max_bound.z) / 2);
     // Bottom
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(node.min_bound.x, node.min_bound.y, mid.z),
-            glm::vec3(mid.x, mid.y, node.max_bound.z)
-        )
+    Node new_node;
+    create_child_node(
+        new_node,
+        glm::vec3(node.min_bound.x, node.min_bound.y, mid.z),
+        glm::vec3(mid.x, mid.y, node.max_bound.z)
     );
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(node.min_bound.x, node.min_bound.y, node.min_bound.z),
-            glm::vec3(mid.x, mid.y, mid.z)
-        )
+    node.children.push_back(new_node);
+    create_child_node(
+        new_node,
+        glm::vec3(node.min_bound.x, node.min_bound.y, node.min_bound.z),
+        glm::vec3(mid.x, mid.y, mid.z)
     );
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(mid.x, node.min_bound.y, node.min_bound.z),
-            glm::vec3(node.max_bound.x, mid.y, mid.z)
-        )
+    node.children.push_back(new_node);
+    create_child_node(
+        new_node,
+        glm::vec3(mid.x, node.min_bound.y, node.min_bound.z),
+        glm::vec3(node.max_bound.x, mid.y, mid.z)
     );
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(mid.x, node.min_bound.y, mid.z),
-            glm::vec3(node.max_bound.x, mid.y, node.max_bound.z)
-        )
+    node.children.push_back(new_node);
+    create_child_node(
+        new_node,
+        glm::vec3(mid.x, node.min_bound.y, mid.z),
+        glm::vec3(node.max_bound.x, mid.y, node.max_bound.z)
     );
+    node.children.push_back(new_node);
+
     // Top
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(node.min_bound.x, mid.y, mid.z),
-            glm::vec3(mid.x, node.max_bound.y, node.max_bound.z)
-        )
+    create_child_node(
+        new_node,
+        glm::vec3(node.min_bound.x, mid.y, mid.z),
+        glm::vec3(mid.x, node.max_bound.y, node.max_bound.z)
     );
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(node.min_bound.x, mid.y, node.min_bound.z),
-            glm::vec3(mid.x, node.max_bound.y, mid.z)
-        )
+    node.children.push_back(new_node);
+    create_child_node(
+        new_node,
+        glm::vec3(node.min_bound.x, mid.y, node.min_bound.z),
+        glm::vec3(mid.x, node.max_bound.y, mid.z)
     );
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(mid.x, mid.y, node.min_bound.z),
-            glm::vec3(node.max_bound.x, node.max_bound.y, mid.z)
-        )
+    node.children.push_back(new_node);
+    create_child_node(
+        new_node,
+        glm::vec3(mid.x, mid.y, node.min_bound.z),
+        glm::vec3(node.max_bound.x, node.max_bound.y, mid.z)
     );
-    node.children.push_back(
-        create_child_node(
-            glm::vec3(mid.x, mid.y, mid.z),
-            glm::vec3(node.max_bound.x, node.max_bound.y, node.max_bound.z)
-        )
+    node.children.push_back(new_node);
+    create_child_node(
+        new_node,
+        glm::vec3(mid.x, mid.y, mid.z),
+        glm::vec3(node.max_bound.x, node.max_bound.y, node.max_bound.z)
     );
+    node.children.push_back(new_node);
 }
 
-Node Octree::create_child_node(glm::vec3 min_bound, glm::vec3 max_bound) {
-    Node new_node;
+void Octree::create_child_node(Node &new_node, const glm::vec3 &min_bound, const glm::vec3 &max_bound) {
     new_node.min_bound = min_bound;
     new_node.max_bound = max_bound;
-
-    return new_node;
 }
--- srcs/Octree.hpp
+++ srcs/Octree.hpp
@@ -29,11 +29,11 @@ class Octree {
         Octree(glm::vec3 min_3d_coord, glm::vec3 max_3d_coord);
         ~Octree();
 
-        bool is_point_inside(Node &node, glm::vec3 position);
+        bool is_point_inside(Node &node, const glm::vec3 &position);
         void insert(std::vector<glm::vec3> &position, std::vector<float> &mass);
-        void insert_point(Node &node, glm::vec3 position, float mass);
+        void insert_point(Node &node, const glm::vec3 &position, float mass);
         void subdivide(Node &node);
-        Node create_child_node(glm::vec3 min_bound, glm::vec3 max_bound);
+        void create_child_node(Node &new_node, const glm::vec3 &min_bound, const glm::vec3 &max_bound);
 };
 
 #endif
